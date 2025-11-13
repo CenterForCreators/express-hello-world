f70103eef351ea61d4e2df422d091b1edb3a8a48
Center for Creators <101010199+CenterForCreators@users.noreply.github.com>
2025-11-12 20:30:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ app.use(cors({
   "https://gateway.pinata.cloud",
   "https://ipfs.io",
   "https://cloudflare-ipfs.com",
-  "https://dweb.link",
-  "https://bafkreifegc6xxg565zwa4dsnnvgyar3gb3afyrysw7q44kelt2jfb7qpvq.ipfs.dweb.link",
-  "https://bafkreifegc6xxg565zwa4dsnnvgyar3gb3afyrysw7q44kelt2jfb7qpvq.ipfs.cf-ipfs.com"
+  "https://dweb.link"
 ],
 methods: ["GET", "POST"],
 allowedHeaders: ["Content-Type"]
